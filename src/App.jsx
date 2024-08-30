897d0dedaae54dd142913509965f075cbd2ab666
Devansh Upadhyay <146647168+upadhyayansh11@users.noreply.github.com>
2024-08-30 16:28:13 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import "./index.css";
+import Title from "./Title";
+import ProductTab from "./ProductTab";
+function App() {
+  return <ProductTab />;
+}
+
+export default App;
---
+++ src/Title.jsx
@@ -0,0 +1,10 @@
+function Title() {
+  let name = "Devansh";
+  return (
+    <div>
+      <p>2*2 = {2 * 2}</p>
+      <p>Hi , {name.toLocaleUpperCase()}</p>
+    </div>
+  );
+}
+export default Title;
